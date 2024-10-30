c0d84588e5c2b4d0c1c7a4e81c99f6e75520f800
seanderik <153625345+SeanDerik@users.noreply.github.com>
2024-10-29 21:07:08 -0300

    Add files via upload

---
+++ github/src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css'
+import { Mapa } from './pages/Mapa';
+
+function App() {
+
+  return (
+      <p>aaaaaaaaaaaaaaaa</p>
+  )
+}
+
+export default App
---
+++ github/src/obj/Ponto2.jsx
@@ -0,0 +1,12 @@
+import pontoimage from '../assets/pontoimage.png';
+
+function Ponto2() {
+
+  return (
+    <>
+      <img src={pontoimage} alt="a plant" />
+    </>
+  )
+}
+
+export default Ponto2
